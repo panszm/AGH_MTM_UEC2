fdbdd575135e82b3f2fbc4a32ce175f8d7df1259
panszm <wr.swider@gmail.com>
2023-04-13 23:29:05 +0200

    Revert "test"
    This reverts commit b180beb0bac773a4264d279b88cde170592e0d0f.

--- lab4_imgctl/rtl/draw_rect_ctl.sv
+++ lab4_imgctl/rtl/draw_rect_ctl.sv
@@ -11,10 +11,10 @@ output logic [11:0] xpos,
 output logic [11:0] ypos
 );
 
-logic [11:0] xpos_nxt;
-longint ypos_nxt;
-int velocity, velocity_nxt;
+logic [11:0] xpos_nxt, ypos_nxt;
+logic [31:0] velocity, velocity_nxt;
 logic is_dropped, is_dropped_nxt, is_falling, is_falling_nxt;
+logic [27:0] fall_counter, fall_counter_nxt;
 
 localparam VISIBLE_HEIGHT = 600,
 RECT_HEIGHT = 64,
@@ -27,12 +27,14 @@ always_ff @(posedge clk) begin
         is_dropped <= 0;
         is_falling <= 0;
         velocity <= 0;
+        fall_counter <= 28'hfffffff;
     end else begin
         xpos <= xpos_nxt;
-        ypos <= ypos_nxt >> 23;
+        ypos <= ypos_nxt;
         is_dropped <= is_dropped_nxt;
         is_falling <= is_falling_nxt;
         velocity <= velocity_nxt;
+        fall_counter <= fall_counter_nxt;
     end
 end
 
@@ -47,7 +49,7 @@ always_comb begin
         velocity_nxt = velocity + ACCELERATION;
         is_falling_nxt = 1;
     end else begin
-        velocity_nxt = velocity - ACCELERATION;
+        velocity_nxt = velocity >> 1;
         is_falling_nxt = 0;
     end;
 
@@ -58,15 +60,26 @@ always_comb begin
     end;
 
     if(is_dropped && velocity != 0) begin
-        if (is_falling) begin
-            ypos_nxt = ypos <<23 + velocity;
+        if(ypos + (velocity >> 25) > VISIBLE_HEIGHT - RECT_HEIGHT && !fall_counter[23]) begin
+            ypos_nxt = VISIBLE_HEIGHT - RECT_HEIGHT;
+            fall_counter_nxt = 28'hfffffff;
+        end else if(!fall_counter[23]) begin
+            if (is_falling) begin
+                ypos_nxt = ypos + (velocity >> 25);
+            end else begin
+                ypos_nxt = ypos - (velocity >> 25);
+            end
+            fall_counter_nxt = 28'hfffffff;
         end else begin
-            ypos_nxt = ypos <<23 - velocity;
-        end
+            ypos_nxt = ypos;
+            fall_counter_nxt = fall_counter - 1;
+        end;
     end else if (is_dropped) begin
-        ypos_nxt = ypos << 23;
+        ypos_nxt = ypos;
+        fall_counter_nxt = fall_counter - 1;
     end else begin
-        ypos_nxt = mouse_y_position << 23;
+        ypos_nxt = mouse_y_position;
+        fall_counter_nxt = fall_counter;
     end;
 end;
 
