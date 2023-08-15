c336e9e466d74656b5add19e11596f9f5bb12691
panszm <wr.swider@gmail.com>
2023-08-15 16:02:07 +0200

    projekt - init

---
+++ projekt/fpga/scripts/project_details.tcl
@@ -0,0 +1,63 @@
+# Copyright (C) 2023  AGH University of Science and Technology
+# MTM UEC2
+# Author: Piotr Kaczmarczyk
+#
+# Description:
+# Project detiles required for generate_bitstream.tcl
+# Make sure that project_name, top_module and target are correct.
+# Provide paths to all the files required for synthesis and implementation.
+# Depending on the file type, it should be added in the corresponding section.
+# If the project does not use files of some type, leave the corresponding section commented out.
+
+#-----------------------------------------------------#
+#                   Project details                   #
+#-----------------------------------------------------#
+# Project name                                  -- EDIT
+set project_name vga_project
+
+# Top module name                               -- EDIT
+set top_module top_vga_basys3
+
+# FPGA device
+set target xc7a35tcpg236-1
+
+#-----------------------------------------------------#
+#                    Design sources                   #
+#-----------------------------------------------------#
+# Specify .xdc files location                   -- EDIT
+set xdc_files {
+    constraints/top_vga_basys3.xdc
+    constraints/clk_wiz_0.xdc
+}
+
+# Specify SystemVerilog design files location   -- EDIT
+set sv_files {
+    ../rtl/vga_pkg.sv
+    ../rtl/vga_if.sv
+    ../rtl/vga_timing.sv
+    ../rtl/draw_bg.sv
+    ../rtl/draw_rect.sv
+    ../rtl/draw_rect_ctl.sv
+    ../rtl/top_vga.sv
+    ../rtl/draw_mouse.sv
+    ../rtl/image/image_rom.sv
+    rtl/top_vga_basys3.sv
+}
+
+# Specify Verilog design files location         -- EDIT
+set verilog_files {
+    ../rtl/clk_wiz_0_clk_wiz.v
+    ../rtl/clk_wiz_0.v
+}
+
+# Specify VHDL design files location            -- EDIT
+set vhdl_files {
+   ../rtl/mouse/MouseCtl.vhd
+   ../rtl/mouse/Ps2Interface.vhd
+   ../rtl/mouse/MouseDisplay.vhd
+}
+
+# Specify files for a memory initialization     -- EDIT
+set mem_files {
+   ../rtl/image/image_rom.data
+}
---
+++ projekt/rtl/top_vga.sv
@@ -0,0 +1,144 @@
+/**
+ * San Jose State University
+ * EE178 Lab #4
+ * Author: prof. Eric Crabilla
+ *
+ * Modified by:
+ * 2023  AGH University of Science and Technology
+ * MTM UEC2
+ * Piotr Kaczmarczyk
+ *
+ * Description:
+ * The project top module.
+ */
+
+`timescale 1 ns / 1 ps
+
+module top_vga (
+    input  logic clk,
+    input  logic clk100MHz,
+    input  logic rst,
+    inout  logic ps2_clk,
+    inout  logic ps2_data,
+    output logic vs,
+    output logic hs,
+    output logic [3:0] r,
+    output logic [3:0] g,
+    output logic [3:0] b
+);
+
+
+/**
+ * Local variables and signals
+ */
+
+// VGA signals from timing
+wire [10:0] vcount_tim, hcount_tim;
+wire vsync_tim, hsync_tim;
+wire vblnk_tim, hblnk_tim;
+
+// VGA signals from background
+vga_bus bus_bg();
+
+// VGA signals from rect
+vga_bus bus_rect();
+
+// VGA signals from mouse
+vga_bus bus_mouse();
+
+/**
+ * Signals assignments
+ */
+
+assign vs = bus_mouse.vsync;
+assign hs = bus_mouse.hsync;
+assign {r,g,b} = bus_mouse.rgb;
+
+
+/**
+ * Submodules instances
+ */
+
+vga_timing u_vga_timing (
+    .clk,
+    .rst,
+    .vcount (vcount_tim),
+    .vsync  (vsync_tim),
+    .vblnk  (vblnk_tim),
+    .hcount (hcount_tim),
+    .hsync  (hsync_tim),
+    .hblnk  (hblnk_tim)
+);
+
+draw_bg u_draw_bg (
+    .clk,
+    .rst,
+
+    .vcount_in  (vcount_tim),
+    .vsync_in   (vsync_tim),
+    .vblnk_in   (vblnk_tim),
+    .hcount_in  (hcount_tim),
+    .hsync_in   (hsync_tim),
+    .hblnk_in   (hblnk_tim),
+
+    .bus_out    (bus_bg.OUT)
+);
+
+logic mouse_left;
+logic [11:0] mouse_x_position, mouse_y_position, rect_x_position, rect_y_position;
+
+MouseCtl mouse_ctl(
+    .clk(clk),
+    .rst,
+    .left(mouse_left),
+    .xpos(mouse_x_position),
+    .ypos(mouse_y_position),
+    .ps2_clk(ps2_clk),
+    .ps2_data(ps2_data)
+);
+
+
+logic [11:0] img_address, img_rgb;
+
+image_rom u_image_rom (
+    .clk,
+    .address(img_address),
+    .rgb(img_rgb)
+);
+
+draw_rect_ctl u_draw_rect_ctl (
+    .clk,
+    .rst,
+    .mouse_left(mouse_left),
+    .mouse_x_position(mouse_x_position),
+    .mouse_y_position(mouse_y_position),
+    .xpos(rect_x_position),
+    .ypos(rect_y_position)
+);
+
+draw_rect u_draw_rect (
+    .clk,
+    .rst,
+
+    .rect_x_position(rect_x_position),
+    .rect_y_position(rect_y_position),
+
+    .bus_in     (bus_bg.IN),
+    .bus_out    (bus_rect.OUT),
+
+    .pixel_addr(img_address),
+    .rgb_pixel(img_rgb)
+);
+
+draw_mouse u_draw_mouse(
+    .clk,
+    .rst,
+
+    .rect_x_position(mouse_x_position),
+    .rect_y_position(mouse_y_position),
+
+    .bus_in     (bus_rect.IN),
+    .bus_out    (bus_mouse.OUT)
+);
+
+endmodule
